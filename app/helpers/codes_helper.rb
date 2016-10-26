aaa54e5633ac6aaff6786725d79a59e8563973fe
Samir Undavia <samir314159@gmail.com>
2016-10-26 00:26:46 -0400

    get personal code from codes table in get tiqued

--- app/helpers/codes_helper.rb
+++ app/helpers/codes_helper.rb
@@ -1,7 +1,7 @@
 module CodesHelper
     def my_codes
         if logged_in?
-            Code.where(owner: current_user.id)
+            Code.where(owner: current_user.id).where.not(code_type:'personal')
         end
     end
 end
